66688602786ef0c60776fdff72d2104f36ea9062
dush valentin <codesvalentin@gmail.com>
2024-08-13 23:01:05 +0200

    upd: storage init

--- app/chat/chatSection.tsx
+++ app/chat/chatSection.tsx
@@ -3,6 +3,8 @@ import { Button } from "@nextui-org/button"
 import { User } from "firebase/auth"
 import Image from "next/image"
 import { BsSend } from "react-icons/bs"
+import { FaRegImage } from "react-icons/fa6";
+import { FaMicrophone } from "react-icons/fa";
 
 export default function ChatSection({ activeReceiver, currentUser, messages, message, setMessage, handleSendMessage, handleKeyPress, messagesEndRef, setShowUsers }: {
     activeReceiver: IUser | null,
@@ -42,9 +44,8 @@ export default function ChatSection({ activeReceiver, currentUser, messages, mes
             <div className="flex-grow max-h-[calc(100vh-200px)] p-4 overflow-auto">
                 {messages.map((msg) => (
                     <div key={msg.id} className={`mb-2 ${msg.sender === currentUser?.uid ? 'text-right' : 'text-left'}`}>
-                        <div className={`inline-block p-2 rounded-lg ${
-                            msg.sender === currentUser?.uid ? 'bg-blue-500 text-white' : 'bg-gray-200 text-black dark:bg-gray-700 dark:text-white'
-                        }`}>
+                        <div className={`inline-block p-2 rounded-lg ${msg.sender === currentUser?.uid ? 'bg-blue-500 text-white' : 'bg-gray-200 text-black dark:bg-gray-700 dark:text-white'
+                            }`}>
                             {msg.text}
                         </div>
                     </div>
@@ -53,6 +54,23 @@ export default function ChatSection({ activeReceiver, currentUser, messages, mes
             </div>
             <div className="p-4 border-t border-gray-200 dark:border-gray-700">
                 <div className="flex items-center gap-2">
+                    <div className="">
+                        <button
+                            className="p-4 bg-gray-800 rounded-xl"
+                        >
+                            <FaMicrophone size={20} />
+                        </button>
+                    </div>
+                    <div className="">
+                        <label htmlFor="image">
+                            <button
+                                className="p-4 bg-gray-800 rounded-xl"
+                            >
+                                <FaRegImage size={20} />
+                            </button>
+                        </label>
+                        <input type="file" name="image" className="hidden" id="image" />
+                    </div>
                     <textarea
                         value={message}
                         onChange={(e) => setMessage(e.target.value)}
@@ -61,8 +79,8 @@ export default function ChatSection({ activeReceiver, currentUser, messages, mes
                         placeholder="Type your message..."
                         rows={2}
                     />
-                    <Button 
-                        onClick={handleSendMessage} 
+                    <Button
+                        onClick={handleSendMessage}
                         color="primary"
                         className="py-8"
                     >
